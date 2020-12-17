9264ec3d00e2ca97fbc7a63a188fb2e2b76654ee
Paul McClean <wutangpaul@gmail.com>
2020-12-17 00:58:33 +0000

    Initial commit of skeleton svelte/sapper app with SASS installed

---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<style>
+	h1,
+	p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Svelte 🚀</title>
+</svelte:head>
+
+<h1>Flarg!</h1>
+
+<p>This is a flargagraph</p>
